510ee146c7f673040a87814d8d7678b3fcc88312
selias1 <selias1@uci.edu>
2017-11-13 17:13:54 -0800

    commit projects
    edit projects

---
+++ script.js
@@ -0,0 +1,20 @@
+<!--
+<script href = "script.js" type="text/javascript"></script>
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
+
+-->
